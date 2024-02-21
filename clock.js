0e3065db328c9a36f940c9b2a1937a03f698396f
Abu-Sayed-Sarker <abusayed96728@gmail.com>
2024-02-22 00:13:10 +0600

    digital clock

---
+++ clock.js
@@ -0,0 +1,18 @@
+function currentTime(){
+    var dateTime = new Date();
+    var hrs = dateTime.getHours();
+    var min = dateTime.getMinutes();
+    var sec = dateTime.getSeconds();
+    var am_pm = document.getElementById(am_pm);
+
+    if(hrs >= 12){
+        return 1;
+
+    }
+
+    document.getElementById("hrs").innerHTML = hrs;
+    document.getElementById("min").innerHTML = min;
+    document.getElementById("sec").innerHTML = sec;
+
+}
+setInterval(currentTime, 1000);
