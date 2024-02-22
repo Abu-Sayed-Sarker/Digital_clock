ad905dd91e90c0c5b57ee76522a59d4592ee40f3
Abu-Sayed-Sarker <abusayed96728@gmail.com>
2024-02-22 20:52:43 +0600

    Condation Add

--- clock.js
+++ clock.js
@@ -1,18 +1,35 @@
-// function currentTime(){
-//     var dateTime = new Date();
-//     var hrs = dateTime.getHours();
-//     var min = dateTime.getMinutes();
-//     var sec = dateTime.getSeconds();
-//     var am_pm = document.getElementById(am_pm);
+function currentTime(){
+    var dateTime = new Date();
+    var hrs = dateTime.getHours();
+    var min = dateTime.getMinutes();
+    var sec = dateTime.getSeconds();
+    var am_pm = document.getElementById("am_pm");
 
-//     if(hrs >= 12){
-//         return 1;
+    if(hrs >= 12){
+        am_pm.innerHTML = "PM";
+    } 
+    else{
+        am_pm.innerHTML = "AM";
+    }
 
-//     }
+    if(hrs > 12){
+        hrs = hrs - 12;
+    }
+    
+    if(sec < 12){
+        sec = "0" + sec;
+    }
+    if(min < 12){
+        min = "0" + min;
+    }
+    if(hrs < 12){
+        hrs = "0" + hrs;
+    }
 
-//     document.getElementById("hrs").innerHTML = hrs;
-//     document.getElementById("min").innerHTML = min;
-//     document.getElementById("sec").innerHTML = sec;
 
-// }
-// setInterval(currentTime, 1000);
+    document.getElementById("hrs").innerHTML = hrs;
+    document.getElementById("min").innerHTML = min;
+    document.getElementById("sec").innerHTML = sec;
+
+}
+setInterval(currentTime, 1000);
